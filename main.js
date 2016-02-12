277e607979fea4f53550168c8ea59d4d0bd70c72
Roland Vachter <roland.vachter@iquestgroup.com>
2016-02-12 11:30:33 +0200

    Initial commit.

---
+++ main.js
@@ -0,0 +1,89 @@
+const config = require('./src/javascripts/config.js');
+const oCommentApi = require('o-comment-api');
+const defaultConfig = require('./config.json');
+const oCommentUtilities = require('o-comment-utilities');
+const Widget = require('./src/javascripts/Widget.js');
+
+/**
+ * Default config (prod) is set.
+ */
+config.set(defaultConfig);
+
+/**
+ * Enable data caching.
+ */
+oCommentApi.setConfig('cache', true);
+
+
+/**
+ * Widget.js exposed as main constructor
+ * @type {object}
+ */
+module.exports = Widget;
+
+/**
+ * Adds or overrides configuration options.
+ *
+ * @param  {string|object} keyOrObject Key or actually an object with key-value pairs.
+ * @param  {anything} value Optional. Should be specified only if keyOrObject is actually a key (string).
+ * @return {undefined}
+ */
+module.exports.setConfig = function () {
+	config.set.apply(this, arguments);
+}
+
+module.exports.init = function (el) {
+	return oCommentUtilities.initDomConstruct({
+		context: el,
+		classNamespace: 'o-comment-count',
+		eventNamespace: 'oCommentCount',
+		module: module.exports
+	});
+};
+module.exports.utilities = oCommentUtilities;
+module.exports.dataService = oCommentApi;
+
+/**
+ * Enables logging.
+ * @return {undefined}
+ */
+module.exports.enableLogging = function () {
+	oCommentUtilities.logger.enable.apply(this, arguments);
+};
+
+/**
+ * Disables logging.
+ * @return {undefined}
+ */
+module.exports.disableLogging = function () {
+	oCommentUtilities.logger.disable.apply(this, arguments);
+};
+
+/**
+ * Sets logging level.
+ * @return {undefined}
+ */
+module.exports.setLoggingLevel = function () {
+	oCommentUtilities.logger.setLevel.apply(this, arguments);
+};
+
+
+document.addEventListener('o.DOMContentLoaded', function () {
+	try {
+		const configInDomEl = document.querySelector('script[type="application/json"][data-o-comment-count-config]');
+		if (configInDomEl) {
+			const configInDom = JSON.parse(configInDomEl.innerHTML);
+
+			module.exports.setConfig(configInDom);
+		}
+	} catch (e) {
+		oCommentUtilities.logger.log('Invalid config in the DOM.', e);
+	}
+
+	oCommentUtilities.initDomConstruct({
+		classNamespace: 'o-comment-count',
+		eventNamespace: 'oCommentCount',
+		module: module.exports,
+		auto: true
+	});
+});
---
+++ src/javascripts/Widget.js
@@ -0,0 +1,99 @@
+const envConfig = require('./config.js');
+const oCommentUtilities = require('o-comment-utilities');
+const oCommentApi = require('o-comment-api');
+
+const Widget = function (rootEl, config) {
+	let widgetEl;
+
+	try {
+		if (!rootEl) {
+			rootEl = document.body;
+		} else if (!(rootEl instanceof HTMLElement)) { // could throw exception in IE
+			rootEl = document.querySelector(rootEl);
+		}
+	} catch (e) {
+		let el;
+		if (typeof rootEl === 'string') {
+			el = document.querySelector(rootEl);
+		}
+
+		if (el) {
+			rootEl = el;
+		} else {
+			rootEl = document.body;
+		}
+	}
+
+	rootEl.setAttribute('data-o-comment-count-js', '');
+
+	widgetEl = rootEl;
+
+
+	/**
+	 * Validation of the initial configuration object.
+	 */
+	if (!config) {
+		throw new Error("No configuration is provided.");
+	}
+
+	if (!config.articleId) {
+		if (!config.articleid) {
+			throw new Error("Article ID is missing.");
+		} else {
+			config.articleId = config.articleid;
+		}
+	}
+
+	let template = envConfig.template;
+	if (config.template) {
+		template = config.template;
+	}
+
+
+	function render () {
+		getCommentCount(function (err, count) {
+			if (err) {
+				return;
+			}
+
+			widgetEl.innerHTML = template.replace('{count}', count).replace('{plural}', (count > 1 ? 's' : ''));
+		});
+	}
+
+	function getCommentCount (callback) {
+		oCommentApi.getCommentCount(config.articleId, function (err, count) {
+			if (err) {
+				callback(err);
+
+				oCommentUtilities.logger.error('Error fetching the comment count', err);
+				return;
+			}
+
+			callback(null, count);
+		});
+	}
+
+	let refreshInitialized = false;
+	function init () {
+		render();
+
+		if (config.autoRefresh && !refreshInitialized) {
+			refreshInitialized = true;
+
+			setTimeout(function () {
+				render();
+			}, 5 * 60 * 1000);
+		}
+	}
+
+	if (config.autoInit !== false) {
+		init();
+	}
+
+	return {
+		init: init,
+		getCommentCount: getCommentCount
+	};
+};
+
+module.exports = Widget;
